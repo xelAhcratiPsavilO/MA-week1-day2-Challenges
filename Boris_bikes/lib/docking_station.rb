8744bd7aab80c02364c66de505bebc5f01094387
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-02-22 12:59:08 +0000

    Add method for docking station capacity

--- Boris_bikes/lib/docking_station.rb
+++ Boris_bikes/lib/docking_station.rb
@@ -1,12 +1,26 @@
 class DockingStation
   attr_reader :bike
+  attr_reader :arr
 
   def release_bike
     Bike.new
   end
 
-  def dock(bike)
+  def getting_storage
+    arr = []
+  end
+
+  def dock(bike, storage)
     @bike = bike
+    @arr = storage
+    if @arr.length == 0 || @arr.length == 1
+      @arr.push(@bike)
+      puts "This bike is now in the station"
+      p @arr
+    else
+      puts "Sorry, the capacity of the station was already filled"
+      p @arr
+    end
   end
 
   def bike_available?
