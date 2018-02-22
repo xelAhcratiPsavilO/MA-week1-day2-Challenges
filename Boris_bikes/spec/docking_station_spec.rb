6c3c68a00ab31f7ddcaf5d54baca3442c5811166
Cindy <jl7e12@gmail.com>
2018-02-22 14:31:41 +0000

    unit test for method dock passed

--- Boris_bikes/spec/docking_station_spec.rb
+++ Boris_bikes/spec/docking_station_spec.rb
@@ -2,11 +2,12 @@ require './lib/docking_station.rb'
 
 describe DockingStation do
   bike = Bike.new
+  storage = []
 
   it { should respond_to(:release_bike, :bike_available?) }
   it { expect(subject.release_bike).to be_a Bike }
   it { expect(subject.release_bike.working?).to eq true }
-  it { should respond_to(:dock).with(1).argument }
-  it { expect(subject.dock(bike)).to eq bike }
+  it { should respond_to(:dock).with(2).argument }
+  it { expect(subject.dock(bike, storage)).to match [bike] }
   it { expect { subject.bike_available? }.to raise_error("Sorry, no bike in the dock") }
 end
